058a2fd0b5e2ebc37b2005b853758ed8e9d48367
vtralci <144631973+vtralci@users.noreply.github.com>
2025-10-08 15:47:16 +0200

    adjacency rules

---
+++ main.py
@@ -0,0 +1,17 @@
+from pyDatalog import pyDatalog
+
+pyDatalog.create_terms('X, Y, X1, Y1, adjacent, box, wumpus, pit, gold, breeze, stench')
+adjacent(X, Y, X1, Y1) <= (box(X, Y) & box(X1, Y1) & ((X1 == X + 1) & (Y1 == Y)))
+adjacent(X, Y, X1, Y1) <= (box(X, Y) & box(X1, Y1) & ((X1 == X - 1) & (Y1 == Y)))
+adjacent(X, Y, X1, Y1) <= (box(X, Y) & box(X1, Y1) & ((X1 == X) & (Y1 == Y + 1)))
+adjacent(X, Y, X1, Y1) <= (box(X, Y) & box(X1, Y1) & ((X1 == X) & (Y1 ==  Y - 1)))
+
+for i in range(0, 5):
+    for j in range(0, 5):
+        +box(i+1, j+1)
+print(f"Celdas: {box(X, Y).data}")
+
+
+print(bool(adjacent(1, 1, 1, 2)))  # True
+print(bool(adjacent(X, Y, 1, 1)))  # False
+print(f"Adyacencias: {adjacent(X, Y, X1, X2).data}")
