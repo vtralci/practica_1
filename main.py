f4209f53c99cb17a6805fa7887ad7f072f421e20
vtralci <144631973+vtralci@users.noreply.github.com>
2025-10-09 15:33:36 +0200

    safety rules

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from pyDatalog import pyDatalog
 
-pyDatalog.create_terms('X, Y, X1, Y1, adjacent, box, wumpus, pit, gold, breeze, stench')
+pyDatalog.create_terms('X, Y, X1, Y1, adjacent, box, shine, stench, safe')
+#adjacency rules
 adjacent(X, Y, X1, Y1) <= (box(X, Y) & box(X1, Y1) & ((X1 == X + 1) & (Y1 == Y)))
 adjacent(X, Y, X1, Y1) <= (box(X, Y) & box(X1, Y1) & ((X1 == X - 1) & (Y1 == Y)))
 adjacent(X, Y, X1, Y1) <= (box(X, Y) & box(X1, Y1) & ((X1 == X) & (Y1 == Y + 1)))
 adjacent(X, Y, X1, Y1) <= (box(X, Y) & box(X1, Y1) & ((X1 == X) & (Y1 ==  Y - 1)))
 
+#safety rules
+safe(X, Y) <= box(X, Y) & breeze(X, Y)
+safe(X, Y) <= box(X, Y) & shine(X, Y)
+safe(X, Y) <= box(X, Y) & stench(X, Y)
+
+#
+
+#creating a 5x5 grid
 for i in range(0, 5):
     for j in range(0, 5):
         +box(i+1, j+1)
-print(f"Celdas: {box(X, Y).data}")
+print(f"Casillas: {box(X, Y).data}")
 
 
 print(bool(adjacent(1, 1, 1, 2)))  # True
